d925da0ae2cb51df4086ca24964ec5689222d087
Aadesh2019 <aadeshcabservice@gmail.com>
2025-03-06 15:16:29 +0530

    Add files via upload

---
+++ aComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <built-comp
+  :entity_id="entity_id" :props="props" @customevent="customevent">
+  <slot></slot>
+  </built-comp>
+  </template>
+  <script>
+  export default {
+    props: ['entity_id', 'props'],
+    methods:{
+      customevent(e) {this.$emit('customevent',e)},
+    }
+  }
+  </script>
